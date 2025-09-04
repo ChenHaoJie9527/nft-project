fa8c271e808cc3539239493a83f1e4b42fe4b3bf
momotom <a18520145493@163.com>
2025-09-04 11:15:46 +0800

    feat: 移除Header底部线条

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/utils';
 import ActionButton from './action-button';
 
 const headerVariants = cva(
-  'sticky top-0 z-50 w-full border-border/90 border-b bg-background/95 backdrop-blur transition-transform duration-300 ease-in-out supports-[backdrop-filter]:bg-background/60',
+  'sticky top-0 z-50 w-full bg-background/95 backdrop-blur transition-transform duration-300 ease-in-out supports-[backdrop-filter]:bg-background/60',
   {
     variants: {
       variant: {
